6bc6484605c39f5d52f40c6e6299474f8f099f77
zerontec <zerontecnologia1@gmail.com>
2022-07-08 19:20:49 -0400

    I did user Model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,35 +1,45 @@
-const {DataTypes}= require ('sequelize');
+const { DataTypes } = require("sequelize");
 
-
-module.exports= (sequelize) => {
-
-sequelize.define('user',{
-
+module.exports = (sequelize) => {
+  sequelize.define("user", {
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique:true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accepTerms: { type: DataTypes.BOOLEAN },
+    role: { type: DataTypes.STRING, allowNull: false },
+    verificationToken: { type: DataTypes.STRING },
+
+    verified: { type: DataTypes.DATE },
+    resetToken: { type: DataTypes.STRING },
+    resetTokenExpires: { type: DataTypes.DATE },
+    passwordReset: { type: DatatTypes.DATE },
+
+    created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updated: { type: DataTypes.DATE },
+    isVerified: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return !!(this.verified || this.passwordReset);
       },
-      name:{
-        type:DataTypes.STRING,
-        allowNull:false
-      },
-      email:{
-          type:DataTypes.STRING,
-          allowNull: false,
-          unique:true
-      },
-      password:{
-          type:DataTypes.STRING,
-          allowNull: false,
-      },
-
-
-
-
-
-
-})
-
-
-}
+    },
+  });
+};
